d21c88eda4dcc03c138fd4eecf08cad1587bc9de
Nadusha <nskuznetsova0482@gmail.com>
2025-10-08 15:28:12 +0300

    Добавлена функция подсчета уникальных слов

--- nadya_count.py
+++ nadya_count.py
@@ -2,3 +2,9 @@ def count_words(text):
     words = text.split()
     return len(words)
 print('Rebase practice')
+
+# Функция для подсчета уникальных слов
+def count_uniq_words(text):
+    words = text.split(words)
+    uniq_words = set(words)
+    return len(uniq_words)

937baea75ebd3498814cb4545bdfed2082f0238a
Nadusha <nskuznetsova0482@gmail.com>
2025-10-08 17:20:06 +0300

    Добавлена функция поиска самого длинного слова

--- nadya_count.py
+++ nadya_count.py
@@ -2,3 +2,10 @@ def count_words(text):
     words = text.split()
     return len(words)
 print('Rebase practice')
+
+def find_longest_word(text):
+    """Функция для поиска самого длинного слова"""
+    words = text.split()
+    if not words:
+        return ""
+    return max(words, key=len)

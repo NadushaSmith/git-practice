400b9329da234215a4d033389b759448541e6e05
Smithdata <nskuznetsova0482@gmail.com>
2025-10-09 14:35:26 +0300

    Data/word count (#3)
    * Добавлена функция подсчета уникальных слов
    
    * Add new commit
    
    * Добавлены новые изменения

--- nadya_count.py
+++ nadya_count.py
@@ -1,6 +1,18 @@
+# Функция подсчета слов
 def count_words(text):
     words = text.split()
     return len(words)
+print(count_words)
+
+# Функция для подсчета уникальных слов
+def count_uniq_words(text):
+    words = text.split(words)
+    uniq_words = set(words)
+    return len(uniq_words)
+print(count_uniq_words)
+
+# The and!
+
 print('Rebase practice')
 
 def find_longest_word(text):
@@ -9,3 +21,4 @@ def find_longest_word(text):
     if not words:
         return ""
     return max(words, key=len)
+
